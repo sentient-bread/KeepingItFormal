d3460ccf7a79f52a4a04bdf2e5b8d434707a89b7
Shashwat Singh <singhshashwat1002@gmail.com>
2021-11-24 19:22:52 +0530

    rules 1, 2, and 4

--- evaluator.hs
+++ evaluator.hs
@@ -1,5 +1,16 @@
-import AllTypes 
+import AllTypes ( Model, VarAssmt, LExpr (LCon), Denot(Indv), Index ) 
 
 eval :: LExpr -> Model -> VarAssmt -> Index -> Denot -- evaluate in model
-eval _ _ _ _ = Indv ""
---------------------
+
+--- Rule 1:
+eval (LCon con) model g index = (f con ) ((fst index) (snd index))
+
+--- Rule 2: 
+eval (LVar var) model g index = g var
+
+
+--- Rule 3:
+
+--- Rule 4:
+eval (Appl alpha beta) model g index = (eval alpha model g index) (eval beta model g index)
+
