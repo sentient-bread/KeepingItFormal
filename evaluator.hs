9ee3065920571f38217540a4d23acb44ea9e8967
Shashwat Singh <singhshashwat1002@gmail.com>
2021-11-26 12:38:32 +0530

    fix indentation based syntax issues for the file

--- evaluator.hs
+++ evaluator.hs
@@ -18,9 +18,10 @@ eval (Appl alpha beta) model g index = func (eval beta model g index)
 
 --- Rule 5:
 eval (Eqp alpha beta) model g index
-    | (int_alpha == int_beta) = TVal True where int_alpha = eval alpha model g index
-                                , int_beta = eval beta model g index
+    | (int_alpha == int_beta) = TVal True 
     | otherwise = TVal False
+    where int_alpha = eval alpha model g index
+          int_beta = eval beta model g index
 
 --- Rule 6:
 eval (Not phi) model g index
@@ -32,27 +33,25 @@ eval (And phi psy) model g index
     | int_phi_truth && int_psy_true == True = TVal True
     | otherwise = TVal False 
     where TVal int_phi_truth = eval phi model g index
-    , TVal int_psy_truth = eval phi model g index
+          TVal int_psy_truth = eval phi model g index
 
 --- Rule 8 (or):
 eval (Or phi psy) model g index
     | int_phi_truth || int_psy_truth == True = TVal True
     | otherwise = TVal False
     where TVal int_phi_truth = eval phi model g index
-    , TVal int_psy_truth = eval psy model g index
+          TVal int_psy_truth = eval psy model g index
 
 --- Rule 9 (implication):
 eval (Impl phi psy) model g index
     | (not int_phi_truth) || int_psy_truth == True = TVal True
     | otherwise = TVal False
     where TVal int_phi_truth = eval phi model g index
-    , TVal int_psy_truth = eval psy model g index
+          TVal int_psy_truth = eval psy model g index
 
 --- Rule 10 (double implication):
 eval (Eqv phi psy) model g index
     | fwd_implication && bcwd_implication == True = TVal True
     | otherwise = TVal False
     where TVal fwd_implication = eval (Impl phi psy) model g index
-    , TVal bcwd_implication = eval (Impl psy phi) model g index
-
-
+          TVal bcwd_implication = eval (Impl psy phi) model g index 
