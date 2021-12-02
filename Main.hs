49c1596325c200e08de24fc05381dc5a91c3bc26
Abhinav271828 <abhinav.menon@students.iiit.ac.in>
2021-12-03 03:20:43 +0530

    Output result of evaluation

--- Main.hs
+++ Main.hs
@@ -1,8 +1,11 @@
 import AllTypes
 import Translate
 import Parse
+import Model
+import Evaluator
 
 main = do
       line <- getLine
       let p = (simplify . translate . scanparse) line
       putStrLn $ show p
+      putStrLn $ "The truth value is " ++ (show $ eval p model g (W 1, T 2))
